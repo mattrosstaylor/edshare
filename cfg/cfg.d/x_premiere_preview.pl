71c5fe5e1a86ff09fd9ad3c1e5e690d44440f7d5
Matt R Taylor <matt.ross.taylor@gmail.com>
2013-05-15 18:22:41 +0100

    new document list style - much much better :-)

--- cfg/cfg.d/x_premiere_preview.pl
+++ cfg/cfg.d/x_premiere_preview.pl
@@ -1,6 +1,7 @@
 $c->{premiere_preview_width} = 640;
 $c->{premiere_preview_height} = 480;
 
+
 $c->{plugins}{"Convert::OfficePreview"}{params}{disable} = 0;
 
 # enable audio_*/video_* previews
@@ -9,7 +10,7 @@ $c->{thumbnail_types} = sub {
 	push @$list, qw( pdf );
 };
 
-$c->{render_premiere_preview} = sub
+$c->{render_premiere_preview_area} = sub
 {
 	my ( $eprint, $repository ) = @_;
 	my $xml = $repository->xml;
@@ -28,36 +29,103 @@ document.observe('dom:loaded', function(){
 EOF
 
 
-	my $preview_area = $xml->create_element("iframe", "id"=>"premiere_preview_area", "style"=>"width:".$w."px;height:".$h."px;", "scrolling"=>"no");
-	my $js = $xml->create_element("script", "type"=>"text/javascript");
+	my $preview_area = $xml->create_element("iframe", id=>"premiere_preview_area", style=>"width:".$w."px;height:".$h."px;", "scrolling"=>"no");
+	my $js = $xml->create_element("script", type=>"text/javascript");
 	$js->appendChild($xml->create_text_node($script));
 	$div->appendChild($preview_area);
 	$div->appendChild($js);
 	return $div;
 };
 
+$c->{render_premiere_preview_documents} = sub
+{
+	my ( $eprint, $repository ) = @_;
+	my $xml = $repository->xml;
+
+	my $panel = $xml->create_document_fragment();
+
+	# document count
+	my $document_count_div = $xml->create_element("div", class=>"premiere_preview_right_header", style=>"height:15px;padding:5px;");
+	$panel->appendChild($document_count_div);
+	my $document_count = scalar($eprint->get_all_documents());
+	
+	if ($document_count eq 1)
+	{
+		$document_count_div->appendChild($repository->html_phrase("premiere_preview_document_count_one"));
+	}
+	else
+	{
+		$document_count_div->appendChild(
+			$xml->create_text_node(
+				$repository->phrase("premiere_preview_document_count", count=>$document_count)
+			)
+		);
+	}
+	
+	# document list
+	my $document_list_div = $xml->create_element("div", style=>"overflow-y:scroll;overflow:auto;height:430px;");
+	my $document_list = $xml->create_element("ol", class=>"premiere_preview_document_list");
+	$document_list_div->appendChild($document_list);
+	$panel->appendChild($document_list_div);
+	
+	foreach ($eprint->get_all_documents())
+	{
+		$document_list->appendChild($_->render_citation("premiere_preview_selector"));
+	}
+	foreach ($eprint->get_all_documents())
+	{
+		$document_list->appendChild($_->render_citation("premiere_preview_selector"));
+	}
+	foreach ($eprint->get_all_documents())
+	{
+		$document_list->appendChild($_->render_citation("premiere_preview_selector"));
+	}
+	foreach ($eprint->get_all_documents())
+	{
+		$document_list->appendChild($_->render_citation("premiere_preview_selector"));
+	}
+	foreach ($eprint->get_all_documents())
+	{
+		$document_list->appendChild($_->render_citation("premiere_preview_selector"));
+	}
+
+	# footer
+	my $footer = $xml->create_element("div", class=>"premiere_preview_right_header", style=>"position: relative; bottom:0; height:15px; padding: 5px;");
+	$footer->appendChild( $xml->create_text_node("DOWNLOAD BUTTON GOES HERE"));
+	$panel->appendChild($footer);
+
+	return $panel;
+};
+
+
 $c->{eprint_render} = sub
 {
 	my ( $eprint, $repository, $preview ) = @_;
 
 	my $xml = $repository->xml;
+	my $w = $repository->get_conf("premiere_preview_width");
+	my $h = $repository->get_conf("premiere_preview_height");
+
+
 	my $title = $eprint->render_citation("brief");
 	#my $title = $xml->create_document_fragment;
 
-	my $page = $xml->create_element("div");
+	my $page = $xml->create_element("div", id=>"premiere_preview_container");
 
 	# replace this if they every 
 	my $naughty_css_hack = $xml->create_element("style", type=>"text/css");
 	$naughty_css_hack->appendChild( $xml->create_text_node(".ep_tm_pagetitle { display: none; }"));
 	$page->appendChild($naughty_css_hack);
 
-	my $p = $repository->call("render_premiere_preview", $eprint, $repository);
-	my $d = $eprint->render_citation("premiere_preview_documents");
-	
+	my $left = $xml->create_element("div", id=>"premiere_preview_left", style=>"float:left;width:".$w."px");
+	my $right = $xml->create_element("div", id=>"premiere_preview_right", style=>"margin-left:".$w."px; height:".$h."px;position:relative;");
 	# hmmmm, try and find a way to make these divs the correct way around later...
-	$page->appendChild($d);
-	$page->appendChild($p);
+	$page->appendChild($left);
+	$page->appendChild($right);
 
+	$left->appendChild( $repository->call("render_premiere_preview_area", $eprint, $repository));
+	$right->appendChild( $repository->call("render_premiere_preview_documents", $eprint, $repository));
+		
 	my $links = $repository->xml->create_document_fragment();
 	if( !$preview )
 	{
@@ -66,7 +134,7 @@ $c->{eprint_render} = sub
 	}
 
 	# add the main info
-	$page->appendChild($eprint->render_citation( "premiere_preview_info" ));
+	#$left->appendChild($eprint->render_citation( "premiere_preview_info" ));
 
 	return( $page, $title, $links );
 };
--- lib/plugins/EPrints/Plugin/PremierePreviewScripts.pm
+++ lib/plugins/EPrints/Plugin/PremierePreviewScripts.pm
@@ -6,6 +6,24 @@ our @ISA = qw/ EPrints::Plugin /;
 
 package EPrints::Script::Compiled;
 
+
+sub run_premiere_preview_license_icon
+{
+	my( $self, $state, $object ) = @_;
+	my $repo = $state->{session}->get_repository;
+	my $eprint = $object->[0];
+
+	if( !defined $eprint || ref($eprint) ne "EPrints::DataObj::EPrint" )
+	{
+		$self->runtime_error( "Script '".caller(3) ."' can only be called on an EPrint not ". ref($eprint));
+	}
+
+	my $icon_url = $repo->xml->create_element("img", "class"=>"premiere_preview_license_icon", "src"=>"/style/images/license_icons/".$eprint->value("license").".png");
+
+	return [ $icon_url, "XHTML" ];
+
+}
+
 sub run_premiere_preview_document_count
 {
 	my( $self, $state, $object ) = @_;
