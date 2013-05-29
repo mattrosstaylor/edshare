b6daea748b85cfd27ad786ea94226124a6d53a30
Matt R Taylor <matt.ross.taylor@gmail.com>
2013-05-29 14:24:33 +0100

    general commit before I split edshare core

--- cfg/cfg.d/x_premiere_preview.pl
+++ cfg/cfg.d/x_premiere_preview.pl
@@ -1,5 +1,5 @@
-$c->{premiere_preview_width} = 640;
-$c->{premiere_preview_height} = 480;
+$c->{premiere_preview_width} = 700;
+$c->{premiere_preview_height} = 525;
 
 
 $c->{plugins}{"Convert::OfficePreview"}{params}{disable} = 0;
@@ -34,50 +34,6 @@ EOF
 	return $div;
 };
 
-$c->{render_premiere_preview_documents} = sub
-{
-	my ( $eprint, $repository ) = @_;
-	my $xml = $repository->xml;
-
-	my $panel = $xml->create_document_fragment();
-
-	# document count
-	my $header_div = $xml->create_element("div", class=>"premiere_preview_document_area_header");
-	my $header_span = $xml->create_element("span");
-	$header_div->appendChild($header_span);
-	$panel->appendChild($header_div);
-
-	my $document_count = scalar($eprint->get_all_documents());
-	
-
-	if ($document_count eq 1)
-	{
-		$header_span->appendChild($repository->html_phrase("premiere_preview_document_count_one"));
-	}
-	else
-	{
-		$header_span->appendChild(
-			$xml->create_text_node(
-				$repository->phrase("premiere_preview_document_count", count=>$document_count)
-			)
-		);
-	}
-	
-	# document list
-	my $document_list_div = $xml->create_element("div", class=>"premiere_preview_document_area");
-	my $document_list = $xml->create_element("ol", class=>"premiere_preview_document_list");
-	$document_list_div->appendChild($document_list);
-	$panel->appendChild($document_list_div);
-	
-	foreach ($eprint->get_all_documents())
-	{
-		$document_list->appendChild($_->render_citation("premiere_preview_selector"));
-	}
-
-
-	return $panel;
-};
-
 $c->{eprint_render} = sub
 {
 	my ( $eprint, $repository, $preview ) = @_;
@@ -96,7 +52,6 @@ $c->{eprint_render} = sub
 	$page->appendChild($right);
 
 	$left->appendChild( $repository->call("render_premiere_preview_area", $eprint, $repository));
-	#$left->appendChild( $repository->call("render_premiere_preview_documents", $eprint, $repository));
 	$left->appendChild( $eprint->render_citation("premiere_preview_document_list"));	
 	my $links = $repository->xml->create_document_fragment();
 	if( !$preview )
--- cfg/static/javascript/auto/premiere_preview.js
+++ cfg/static/javascript/auto/premiere_preview.js
@@ -1,5 +1,5 @@
 function setPremierePreview(id) {
-	$('premiere_preview_area').setAttribute('src','/cgi/preview?doc='+id);
+	$('premiere_preview_area').setAttribute('src','/cgi/premiere_preview?doc='+id);
 	$$('.premiere_preview_selected').invoke('removeClassName', 'premiere_preview_selected');
 	$$('li[id=premiere_preview_selector_'+id+']').invoke('addClassName', 'premiere_preview_selected');
 	$('premiere_preview_document_info').update( $('premiere_preview_document_hidden_info_'+id).innerHTML);
