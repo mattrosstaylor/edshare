f4d738c1f23fd2f255a73f691e9f1a6bef4f763c
Matt R Taylor <matt.ross.taylor@gmail.com>
2013-08-08 13:22:18 +0100

    added ajax spinner for loading. moved images to a consistent location

--- cfg/cfg.d/x_premiere_preview.pl
+++ cfg/cfg.d/x_premiere_preview.pl
@@ -20,6 +20,9 @@ $c->{render_premiere_preview} = sub
 	my $eprintid = $eprint->id;
 
 	my $div = $xml->create_element( "div", id=>"premiere_preview_main");
+	$div->appendChild($xml->create_element( "img", src=>"/images/premiere_preview/ajax-loader.gif", class=>"premiere_preview_ajax" ) );	
+
+
 	my $script = <<EOF;
 document.observe('dom:loaded', function(){
 	initPremierePreview($eprintid, $docid);
