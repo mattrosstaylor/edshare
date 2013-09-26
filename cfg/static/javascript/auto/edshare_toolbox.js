3f6cb61d01978af66e2928a56ef1fe6b2463d487
Matt R Taylor <matt.ross.taylor@gmail.com>
2013-09-26 16:23:46 +0100

    changed edshare_toolbox to use POST method instead of GET in order to prevent caching

--- cfg/static/javascript/auto/edshare_toolbox.js
+++ cfg/static/javascript/auto/edshare_toolbox.js
@@ -1,7 +1,7 @@
 /* edshare core javascript */
 function edshare_core_render_toolbox(targetId, eprintid) {
 	new Ajax.Request( eprints_http_cgiroot +"/edshare_toolbox", {
-		method: "get",
+		method: "post",
 		parameters:{'eprintid':eprintid},
 		onSuccess: function(response) {
 			$(targetId).innerHTML = response.responseText;
