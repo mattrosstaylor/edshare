3622299c0369c16400da8bf17a478d96531084f3
Matt R Taylor <matt.ross.taylor@gmail.com>
2013-11-11 20:55:49 +0000

    added a fallback for when no PDF plugin is installed on the client computer.

--- cfg/static/javascript/jquery.media.0.92.js
+++ cfg/static/javascript/jquery.media.0.92.js
@@ -199,7 +199,12 @@ $.fn.media.defaults.players = {
 	},
 	iframe: {
 		name:  'iframe',
-		types: 'html,pdf'
+		types: 'html'
+	},
+	// mrt - add pdf type
+	pdf: {
+		name: 'pdf',
+		types: 'pdf'
 	},
 	silverlight: {
 		name:  'silverlight',
@@ -417,6 +422,19 @@ function generate(el, opts, player) {
 		o.attr('src', opts.src);
 		o.css('backgroundColor', o.bgColor);
 	}
+	// mrt - add pdf "player"
+	else if (player == 'pdf') {
+		// mrt - detect misery!
+		var info = getAcrobatInfo();
+		if(info.acrobat && false){
+			o = $('<iframe' + ' width="' + opts.width + '" height="' + opts.height + '" >');
+			o.attr('src', opts.src);
+		}
+		else {
+			o = $('<div style="text-align:center;padding-top:' +opts.height*0.3 +'px;width=' + opts.width + 'px;"><h1><img style="vertical-align:middle;" src="/images/premiere_preview/unavailable.png">Preview Unavailable</h1><p>No compatible PDF reader plugin could be found on this computer.</p></div>');
+		}
+		o.css('backgroundColor', o.bgColor);
+	}
 	else if (player == 'img') {
 		o = $('<img>');
 		o.attr('src', opts.src);
@@ -484,10 +502,91 @@ function generate(el, opts, player) {
 	var cls = opts.cls ? (' class="' + opts.cls + '"') : '';
 	var $div = $('<div' + id + cls + '>');
 	$el.after($div).remove();
-	(lameIE || player == 'iframe' || player == 'img') ? $div.append(o) : $div.html(a.join(''));
+	// mrt - oh, i think i will regret editing this....
+	(lameIE || player == 'iframe' || player == 'img' || player == 'pdf') ? $div.append(o) : $div.html(a.join(''));
 	if (opts.caption) $('<div>').appendTo($div).html(opts.caption);
 	return $div;
 };
 
 
 })(jQuery);
+
+
+//
+// http://thecodeabode.blogspot.com
+// @author: Ben Kitzelman
+// @license:  FreeBSD: (http://opensource.org/licenses/BSD-2-Clause) Do whatever you like with it
+// @updated: 03-03-2013
+//
+var getAcrobatInfo = function() {
+
+  var getBrowserName = function() {
+    return this.name = this.name || function() {
+      var userAgent = navigator ? navigator.userAgent.toLowerCase() : "other";
+
+      if(userAgent.indexOf("chrome") > -1)        return "chrome";
+      else if(userAgent.indexOf("safari") > -1)   return "safari";
+      else if(userAgent.indexOf("msie") > -1)     return "ie";
+      else if(userAgent.indexOf("firefox") > -1)  return "firefox";
+      return userAgent;
+    }();
+  };
+
+  var getActiveXObject = function(name) {
+    try { return new ActiveXObject(name); } catch(e) {}
+  };
+
+  var getNavigatorPlugin = function(name) {
+    for(key in navigator.plugins) {
+      var plugin = navigator.plugins[key];
+      if(plugin.name == name) return plugin;
+    }
+  };
+
+  var getPDFPlugin = function() {
+    return this.plugin = this.plugin || function() {
+      if(getBrowserName() == 'ie') {
+        //
+        // load the activeX control
+        // AcroPDF.PDF is used by version 7 and later
+        // PDF.PdfCtrl is used by version 6 and earlier
+        return getActiveXObject('AcroPDF.PDF') || getActiveXObject('PDF.PdfCtrl');
+      }
+      else {
+        return getNavigatorPlugin('Adobe Acrobat') || getNavigatorPlugin('Chrome PDF Viewer') || getNavigatorPlugin('WebKit built-in PDF');
+      }
+    }();
+  };
+
+  var isAcrobatInstalled = function() {
+    return !!getPDFPlugin();
+  };
+
+  var getAcrobatVersion = function() {
+    try {
+      var plugin = getPDFPlugin();
+
+      if(getBrowserName() == 'ie') {
+        var versions = plugin.GetVersions().split(',');
+        var latest   = versions[0].split('=');
+        return parseFloat(latest[1]);
+      }
+
+      if(plugin.version) return parseInt(plugin.version);
+      return plugin.name
+      
+    }
+    catch(e) {
+      return null;
+    }
+  }
+
+  //
+  // The returned object
+  // 
+  return {
+    browser:        getBrowserName(),
+    acrobat:        isAcrobatInstalled() ? 'installed' : false,
+    acrobatVersion: getAcrobatVersion()
+  };
+};
